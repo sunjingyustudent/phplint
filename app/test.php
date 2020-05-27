b3fcc510b578cdce243e88a3e6a876d0eee704a6
靖宇孙 <jingyusun@jingyusundeMacBook-Pro.local>
2020-05-27 16:42:22 +0800

    single

---
+++ app/test.php
@@ -0,0 +1,19 @@
+<?php
+
+class SingleTest{
+
+    private static $instance;
+
+    private function __construct() {}
+
+    public static function getInstance(){
+        if (is_object(self::$instance)) {
+            return self::$instance;
+        }
+        self::$instance = new SingleTest();
+        return self::$instance;
+    }
+}
+
+$a = SingleTest::getInstance();
+var_dump($a);
